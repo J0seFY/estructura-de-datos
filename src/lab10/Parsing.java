b2ca125e62ea853dfd065e2339f300441cd1a684
J0seFY <91684529+J0seFY@users.noreply.github.com>
2022-08-26 21:53:31 -0400

    Update Parsing.java

--- src/lab10/Parsing.java
+++ src/lab10/Parsing.java
@@ -6,7 +6,7 @@ import java.util.Stack;
  * 
  * @author Martita
  * 
- * Para m·s info, visitar https://es.stackoverflow.com/questions/26907/infijo-a-posfijo-en-java
+ * Para m√°s info, visitar https://es.stackoverflow.com/questions/26907/infijo-a-posfijo-en-java
  * Y http://informatica.uv.es/iiguia/AED/laboratorio/P6/pr_06_2005.html
  *
  */
@@ -14,9 +14,10 @@ import java.util.Stack;
 public class Parsing {
 	
 	public static String fromInfijoToPostfijo(String infijo) {
-
+                //Se crea una pila para realizar el recorrido
 		Stack<Character> p = new Stack<>();
 		String s  = "";
+                //Se recorre el string del parametro, cuando corresponde a un numero se agrega a la variable s,si es un parwntesis se agrega a la pila
 		for(int i = 0 ;  i < infijo.length() ; i++){
 			char c = infijo.charAt(i);
 			if(isANumber(c)){
@@ -28,15 +29,15 @@ public class Parsing {
 			}
 		}
 
-		return null;
+		return s;
 	}
-	
+	//Verifica si el char es un numero entre 0-9
 	private static boolean isANumber(char symbol) {
 		return symbol == '0' || symbol == '1' || symbol == '2' || symbol == '3' || 
 				symbol == '4' || symbol == '5' || symbol == '6' || symbol == '7' || 
 				symbol == '8' || symbol == '9';
 	}
-	
+	//Retorna un grado de prioridad dependiendo del simbolo
 	private static int prioridad(char symbol) {
 		if(symbol == '(') return 0;
 		if(symbol == '+' || symbol == '-') return 1;
